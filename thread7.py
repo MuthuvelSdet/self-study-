b018163ea75c9c33680d060efa7da05f370199d2
MuthuvelSdet <31058332+MuthuvelSdet@users.noreply.github.com>
2017-08-16 14:07:28 +0530

    Delete thread7.py

--- thread7.py
+++
@@ -1,9 +0,0 @@
-from multiprocessing import Pool
-import os
-
-def f(x):
-    return str(x*x) + "from " + str(os.getpid())
-
-if (__name__=='__main__'):
-    p = Pool(5)
-    print(p.map(f, [1, 2, 3]))
